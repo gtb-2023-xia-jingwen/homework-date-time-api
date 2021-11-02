45711e23fdb475a3510d87b5dc081858a97bcf92
gitqh <quhang19881208@163.com>
2021-11-02 22:11:21 +0800

    [quhang] refactor: make access level to default

--- src/test/java/com/thoughtworks/capability/gtb/Practice2Test.java
+++ src/test/java/com/thoughtworks/capability/gtb/Practice2Test.java
@@ -1,21 +1,22 @@
 package com.thoughtworks.capability.gtb;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import java.time.LocalDate;
-import org.junit.Assert;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 /**
  * @author itutry
  * @create 2020-05-15_17:23
  */
-public class Practice2Test {
+class Practice2Test {
 
   @Test
-  public void test_getNextWorkDate() {
+  void test_getNextWorkDate() {
     LocalDate workDate1 = Practice2.getNextWorkDate(LocalDate.of(2020, 5, 14));
-    Assert.assertEquals("2020-05-15", workDate1.toString());
+    assertEquals("2020-05-15", workDate1.toString());
 
     LocalDate workDate2 = Practice2.getNextWorkDate(LocalDate.of(2020, 5, 15));
-    Assert.assertEquals("2020-05-18", workDate2.toString());
+    assertEquals("2020-05-18", workDate2.toString());
   }
 }
