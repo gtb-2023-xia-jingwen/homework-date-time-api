fa5998e4e98611d76d01a582510b16b2568138dd
Jingwen Xia <amyfor7@gmai.com>
2022-10-20 14:44:15 +0800

    [Jingwen Xia] fix: get days between next labor day

--- src/main/java/com/thoughtworks/capability/gtb/Practice1.java
+++ src/main/java/com/thoughtworks/capability/gtb/Practice1.java
@@ -1,6 +1,7 @@
 package com.thoughtworks.capability.gtb;
 
 import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 
 /**
  * 计算任意日期与下一个劳动节相差多少天
@@ -11,6 +12,9 @@ import java.time.LocalDate;
 public class Practice1 {
 
   public static long getDaysBetweenNextLaborDay(LocalDate date) {
-    return 0;
+    int year = date.getYear();
+    LocalDate laborDate = LocalDate.of(year, 5, 1);
+    if (date.isAfter(laborDate)) laborDate = laborDate.plusYears(1);
+    return ChronoUnit.DAYS.between(date, laborDate);
   }
 }
